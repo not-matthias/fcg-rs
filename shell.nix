58385b11eba0230b558d914f1611a76ca99e4418
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-10-13 14:45:03 +0200

    feat: add shell.nix

---
+++ shell.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell rec {
+    buildInputs = with pkgs; [
+      rustup
+    ];
+    RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
+    HISTFILE = toString ./.history;
+  }
